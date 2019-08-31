f154dd9df0bff8cd67f9b855043ba9e5f05376be
chaupow <hello@chau-nguyen.de>
2019-08-31 21:05:55 +0200

    functional PID

--- src/main.cpp
+++ src/main.cpp
@@ -32,14 +32,17 @@ string hasData(string s) {
 
 int main() {
   uWS::Hub h;
-
   PID pid;
-  /**
-   * TODO: Initialize the pid variable.
-   */
+  double Kp, Ki, Kd;
+
+  Kp = -0.1;
+  Ki = -0.000;
+  Kd = -1.0;
+  pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
+
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -63,6 +66,8 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
